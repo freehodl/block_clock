668d2cf45792b50c62940574b18dbc1eb5d1d7b9
freehodl <iamgriffin@protonmail.com>
2019-10-18 16:08:40 -0700

    First commit

--- blockclock.py
+++ blockclock.py
@@ -2,14 +2,14 @@ from tkinter import *
 import sys
 import subprocess as sp
 
-def tick():
+def block_clock():
 	blocks = sp.getoutput(' bitcoin-cli getblockchaininfo | grep blocks | cut -c 13-18')
 	clock.config(text= blocks)
-	clock.after(6150, tick)
+	clock.after(615, block_clock)
 root = Tk()
 root.title("Block Clock")
 clock = Label(root, font = ("helvetica", 100, "bold"), bg= "orange")
 clock.grid(row=0, column=1)
-tick()
+block_clock()
 root.mainloop()
 
